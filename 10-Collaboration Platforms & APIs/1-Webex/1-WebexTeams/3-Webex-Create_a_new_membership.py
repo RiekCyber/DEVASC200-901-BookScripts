57a111992651bbbde48b62e977209f8b6602f0bc
Eric SEBERT <e.sebert@gmail.com>
2022-10-27 14:23:29 +0000

    Ok Commit

--- 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/3-Webex-Create_a_new_membership.py
+++ 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/3-Webex-Create_a_new_membership.py
@@ -5,12 +5,12 @@ import pprint
 
 URL = "https://webexapis.com/v1/memberships"
 
-PAYLOAD = {"roomId": "Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vMDJiZTJkNTAtNzZiOC0xMWViLThmZmEtNjU4ZGNlYTg4MTMx",
-    "personEmail": "newUser@devasc.com",
-    "personDisplayName": "Cisco DevNet",
+PAYLOAD = {"roomId": "Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vYmI1MDUwMTAtNTU1NC0xMWVkLWEzODEtYmQ3MWI5Nzg3OGE4",
+    "personEmail": "RiekCyber@devasc.com",
+    "personDisplayName": "RiekCyber",
     "isModerator": "false"}
 
-HEADERS = {"Authorization": "Bearer N2VkNzYwZmUtYmU3Mi00ZTJlLTgxOTYtNDdhMDZmM2EyZmEyYWMyZTM0MWEtOGQz_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921", "Content-Type": "application/json"}
+HEADERS = {"Authorization": "Bearer MWVmMDhiM2MtMTgxZi00ZWQwLWI4MDQtYmYyZmExNjQxNDQ1OWRkN2FlMmEtYTU0_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921", "Content-Type": "application/json"}
 
 RESPONSE = requests.request("POST", URL, data=json.dumps(PAYLOAD), headers=HEADERS)
 
