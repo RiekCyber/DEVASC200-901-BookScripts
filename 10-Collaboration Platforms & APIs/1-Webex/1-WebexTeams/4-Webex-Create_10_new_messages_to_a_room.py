445d8ae08678e1526351f2c02d88817ce1d8481f
Eric SEBERT <e.sebert@gmail.com>
2022-10-27 14:45:53 +0000

    OK Commit

--- 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/4-Webex-Create_10_new_messages_to_a_room.py
+++ 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/4-Webex-Create_10_new_messages_to_a_room.py
@@ -6,9 +6,9 @@ import pprint
 URL = "https://webexapis.com/v1/messages"
 
 PAYLOAD = {"roomId" :"Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vYTQ0Yjg5YTAtNzYwNS0xMWViLTkzYTMtYzUxMTI3YzE5YTkz",
-"text" : "This is a test message"}
+"text" : "This is a test message", 'personEmail': 'RiekCyber@DevAsc.net'}
 
-HEADERS = {"Authorization": "Bearer ",
+HEADERS = {"Authorization": "Bearer MWVmMDhiM2MtMTgxZi00ZWQwLWI4MDQtYmYyZmExNjQxNDQ1OWRkN2FlMmEtYTU0_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921",
 "Content-Type": "application/json",}
 
 RESPONSE = []
