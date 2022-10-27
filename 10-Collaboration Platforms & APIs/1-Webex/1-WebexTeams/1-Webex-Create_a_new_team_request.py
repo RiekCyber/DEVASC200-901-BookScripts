6011e68466552133400075de03799316cad52698
Eric SEBERT <e.sebert@gmail.com>
2022-10-27 13:46:27 +0000

    Ok Commit

--- 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/1-Webex-Create_a_new_team_request.py
+++ 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/1-Webex-Create_a_new_team_request.py
@@ -7,7 +7,7 @@ URL = "https://webexapis.com/v1/teams"
 
 PAYLOAD = {"name": "DevNet Associate Certification Team1"}
 
-HEADERS = {"Authorization": "Bearer OTQxMGEyODAtNzFkZC00ZmU0LWI3NWYtYTRlOTA4YmQ5YmI1ZjAzZGE3MzEtZmNm_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921", "Content-Type" : "application/json"}
+HEADERS = {"Authorization": "Bearer MWVmMDhiM2MtMTgxZi00ZWQwLWI4MDQtYmYyZmExNjQxNDQ1OWRkN2FlMmEtYTU0_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921", "Content-Type" : "application/json"}
 
 RESPONSE = requests.request("POST", URL, data=json.dumps(PAYLOAD), headers=HEADERS)
 
