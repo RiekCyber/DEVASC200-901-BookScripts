0cbd889665d6663ec7c787c770969e00d6297cae
Eric SEBERT <e.sebert@gmail.com>
2022-10-27 16:31:49 +0000

    OK Commit

--- 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/4-Webex-Create_10_new_messages_to_a_room.py
+++ 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/4-Webex-Create_10_new_messages_to_a_room.py
@@ -8,7 +8,7 @@ URL = "https://webexapis.com/v1/messages"
 PAYLOAD = {"roomId" :" Y2lzY29zcGFyazovL3VzL1JPT00vYzQ2OTk3NTAtZGIyNy0xMWU1LWI0ZjQtZmJmMjI3Y2ZmYWYz",
 "text" : "This is a test message"}
 
-HEADERS = {"Authorization": "Bearer MWVmMDhiM2MtMTgxZi00ZWQwLWI4MDQtYmYyZmExNjQxNDQ1OWRkN2FlMmEtYTU0_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921",
+HEADERS = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZXZBU0MiLCJuYW1lIjoiZGV2QVNDLWd1ZXN0IiwiaXNzIjoiWTJselkyOXpjR0Z5YXpvdkwzVnlianBVUlVGTk9tVjFMV05sYm5SeVlXd3RNVjlyTDA5U1IwRk9TVnBCVkVsUFRpOHhZMlE0WmpRMU9DMDFaR0V5TFRReU9UUXRPREF5TmkweFlUUm1PV1ZsTkdGbU5EZyIsImV4cCI6MTY2Njg4OTk3N30.kX2yjOpK0H7e927DJn-S6rD5MntevwDMefhqowq4sII",
 "Content-Type": "application/json",}
 
 RESPONSE = []
