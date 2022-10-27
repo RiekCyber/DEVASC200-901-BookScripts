5bd8ff928ebc84d81675393e588dc060956e77a9
Eric SEBERT <e.sebert@gmail.com>
2022-10-27 14:00:15 +0000

    Ok Commit

--- 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/2-Webex-Create_a_new_room_request.py
+++ 10-Collaboration Platforms & APIs/1-Webex/1-WebexTeams/2-Webex-Create_a_new_room_request.py
@@ -6,7 +6,7 @@ import pprint
 
 URL = "https://webexapis.com/v1/rooms"
 PAYLOAD = {"title": "DevAsc Team Room"}
-HEADERS = {"Authorization": "Bearer N2VkNzYwZmUtYmU3Mi00ZTJlLTgxOTYtNDdhMDZmM2EyZmEyYWMyZTM0MWEtOGQz_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921",
+HEADERS = {"Authorization": "Bearer MWVmMDhiM2MtMTgxZi00ZWQwLWI4MDQtYmYyZmExNjQxNDQ1OWRkN2FlMmEtYTU0_PE93_cb22bf91-b84e-4c50-ab49-cd704940b921",
             "Content-Type": "application/json"}
 
 RESPONSE = requests.request("POST", URL, data=json.dumps(PAYLOAD), headers=HEADERS)
